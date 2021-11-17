f4b605025b270825f0c7f2f3a3651a8cf7113e2f
Wildiney Di Masi <wildiney@gmail.com>
2021-11-17 16:29:08 -0300

    first stable version

---
+++ awake.py
@@ -0,0 +1,29 @@
+import pyautogui
+import time
+import sys
+from datetime import datetime
+
+pyautogui.FAILSAFE = False
+
+numMin = 1
+
+if((len(sys.argv) < 2) or sys.argv[1].isalpha() or int(sys.argv[1]) < 1):
+    numMin = 1
+else:
+    numMin = int(sys.argv[1])
+
+while(True):
+    x = 0
+    while(x < numMin):
+        time.sleep(20)
+        x += 1
+        # print(x)
+    for i in range(0, 100):
+        pyautogui.moveTo(i*4, i*4)
+        time.sleep(.25)
+        # print("move to "+str(i))
+    pyautogui.moveTo(1, 1)
+    for i in range(0, 3):
+        pyautogui.press("shift")
+        # print("press shift")
+    print("Movement made at {}".format(datetime.now().time()))
